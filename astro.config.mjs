a2955491a2853fc3f76623e97a76f32d1ab01616
Kevin Ortiz <92760727+KevinorJG@users.noreply.github.com>
2025-09-19 19:45:16 +0000

    Agregando SSR y rutas dinamicas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
-// @ts-check
 import { defineConfig } from "astro/config";
-
+import node from "@astrojs/node";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import flowbiteReact from "flowbite-react/plugin/astro";
@@ -8,11 +7,9 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://devtalles.community",
-  integrations: [
-    react(),
-    flowbiteReact(),
-    sitemap(),
-  ],
+  output: "server",
+  adapter: node({ mode: "standalone" }), // necesario para Render Web Service
+  integrations: [react(), flowbiteReact(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
     server: {
