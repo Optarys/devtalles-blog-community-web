d61a738d1f317e192cd1d5b1ed862fe343e36359
johs7 <roquejohanssen@gmail.com>
2025-09-20 23:08:54 -0600

    Update environment configuration and adjust platform-specific dependencies

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,4 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
+PUBLIC_API_BASE = "https://optarys-devtalles-blog-api.onrender.com"
+PUBLIC_GRAPHQL_URL = "https://optarys-devtalles-blog-api.onrender.com/graphql"
+PUBLIC_API_URL = "https://optarys-devtalles-blog-api.onrender.com"
+PUBLIC_FRONTEND_URL = "https://devtalles-blog-community-web.onrender.com/" 
